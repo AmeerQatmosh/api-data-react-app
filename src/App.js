b565f39704b1c962c5d7097d4d0992df8fd84932
Ameer Qatmosh <ameerqsh@gmail.com>
2023-05-25 12:36:29 +0300

    push-files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import useFetch from "./components/useFatch";
 
-function App() {
+import githubLogo from "./github-logo.png";
+
+const App = () => {
+  const [data] = useFetch("https://api.github.com/users");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <img className="header-logo" src={githubLogo} alt="GitHub Logo" />
+        <h1 className="header-title">GitHub Users</h1>
       </header>
-    </div>
+      <div className="card-container">
+        {data &&
+          data.map((item) => {
+            return (
+              <div className="card" key={item.id}>
+                <img
+                  className="avatar"
+                  src={item.avatar_url}
+                  alt={`Avatar for ${item.login}`}
+                />
+                <p className="login">{item.login}</p>
+                <a
+                  className="profile-link"
+                  href={item.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Profile
+                </a>
+              </div>
+            );
+          })}
+      </div>
+      <footer className="footer">
+        <p className="footer-text">© 2023 Your Website. All rights reserved.</p>
+      </footer>
+    </>
   );
-}
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 export default App;
